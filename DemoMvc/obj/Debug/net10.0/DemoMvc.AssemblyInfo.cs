82e73ae9c05c8797fd47bff67c8be770cc3d02c8
“JennyBlood” <Minhnguyen9456022@gmail.com>
2026-03-10 09:41:26 +0700

    NQM 2221050684 10/3/2026

--- DemoMvc/obj/Debug/net10.0/DemoMvc.AssemblyInfo.cs
+++ DemoMvc/obj/Debug/net10.0/DemoMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dd8dbb66c8ff6224caf61d668e12d9a444dd0e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17ef1a6a138d1cbb948ca208a27b2e57e42f1bd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
